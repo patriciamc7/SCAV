0f841503a7811a9c6fe1caee62596e229ca48d01
Patri <patricia.martinez.cruz.pmn@gmail.com>
2021-11-13 11:54:30 +0100

    jpg

--- P1/dct.py
+++ P1/dct.py
@@ -3,7 +3,7 @@ import cv2
 import numpy as np
 
 if __name__ == '__main__':
-    image = Image.open("bw2.png");
+    image = Image.open("bw.jpg");
     image = np.float32(image);
     dct = cv2.dct(image);
     image_out = np.uint8(dct)*255;
