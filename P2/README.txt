9b4e8e0a79c50177bdb8ea3ab3b72ce918bd9dfa
Patri <patricia.martinez.cruz.pmn@gmail.com>
2021-11-18 17:31:17 +0100

    readme

---
+++ P2/README.txt
@@ -0,0 +1,17 @@
+SCAV P2: Hay 5 diferentes scripts:
+
+-Main: Donde pedimos por consola que script queremos ejecutar y pedimos los
+paramteros de los siguientes scripts.
+
+-Cut: Cortamos N segundos (por consola), el video y es el que vamos a utilizar 
+siempre, ya que tardara menos.
+
+-Histogram: Creamos un histograma en YUV y lo solapamos con ayuda de ffmpeg.
+
+-Resize: Pedimos al usuario por consola que resolucion quiere, y aplicamos
+la escala en esa resolucion.
+
+-Audio: Leemos el audio del video y lo pasamos a lo que el usuario ha escogido
+por consola.
+
+Por otra parte tambien he añadido los videos de las ejecuciones, y los audios.
--- P2/audio.py
+++ P2/audio.py
@@ -1,11 +1,11 @@
 import subprocess
 
 def audio(modo):
-    if (modo == 0):
-       subprocess.call ([ "ffmpeg", "-i"," stereo.flac", "-ac", "1", "mono_ac.flac"])# ac = audio chanel
-    elif(modo == 1):
-        subprocess.call( ["ffmpeg", "-i", " stereo.flac", "-c:a", "1", "mono_aac.aac"])#-c:a codec aac
-    else:
-        print("opcion no valida")
-        exit()
-    print("se ha creado el video en la carpeta")
+   if (modo == 0):
+    subprocess.call ([ "ffmpeg", "-i","cut.mp4", "-ac", "1", "mono_ac.flac"])
+   elif(modo == 1):
+    subprocess.call( ["ffmpeg", "-i", "cut.mp4", "-c:a", "aac", "mono_aac.aac"])
+   else:
+    print("opcion no valida")
+    exit()
+   print("se ha creado el audio en la carpeta")
